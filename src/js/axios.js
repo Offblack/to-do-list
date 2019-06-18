8778b467ecc45d5c9c3909722c236947c1b8d065
Michal Opalka <michalopalka998@gmail.com>
2019-06-18 21:30:50 +0200

    Faked DB.json server

--- src/js/axios.js
+++ src/js/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-   baseURL: 'https://offblack.github.io/to-do-list/db.json'
+   baseURL: 'https://my-json-server.typicode.com/Offblack/to-do-list'
 });
 
 export default instance;
